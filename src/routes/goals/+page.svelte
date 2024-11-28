98fb50f4de15c1ee3a82686fd7d3d62b5f52503a
Juliano-S <piet0061@hz.nl>
2024-11-28 19:50:52 +0100

    added nav elements

--- src/routes/goals/+page.svelte
+++ src/routes/goals/+page.svelte
@@ -1,1 +1,20 @@
 <h1>goals</h1>
+
+<!-- Navigatiebalk -->
+<nav class="navbar">
+    <a href="/shop" class="nav-item" aria-label="Shop">
+        <i class="fas fa-shopping-cart"></i>
+    </a>
+    <a href="/statistics" class="nav-item" aria-label="Statistics">
+        <i class="fas fa-chart-bar"></i>
+    </a>
+    <a href="/leaderboard" class="nav-item" aria-label="Leaderboard">
+        <i class="fas fa-trophy"></i>
+    </a>
+    <a href="/goals" class="nav-item" aria-label="Goals">
+        <i class="fas fa-bullseye"></i>
+    </a>
+    <a href="/settings" class="nav-item" aria-label="Settings">
+        <i class="fas fa-cog"></i>
+    </a>
+</nav>
