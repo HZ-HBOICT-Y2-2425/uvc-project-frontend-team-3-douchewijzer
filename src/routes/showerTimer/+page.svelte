21ea5089a1783ca75a7c3b7d0f3398df3fa66b12
Aidan Bisschops <aidanbisschops@icloud.com>
2024-12-04 18:01:30 +0100

    kleine aanpassingen en de timer aangeroepen op de douche pagina

--- src/routes/showerTimer/+page.svelte
+++ src/routes/showerTimer/+page.svelte
@@ -1,1 +1,15 @@
+<script>
+    import Timer from '$lib/Timer.svelte';
+
+    const handleTimerEnd = () => {
+        console.log('Timer is afgelopen!');
+    };
+</script>
+
 <h1>Shower Timer</h1>
+
+<Timer on:timerEnd={handleTimerEnd} />
+
+<style>
+    @import '$lib/app.css';
+</style>
