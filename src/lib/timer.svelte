4a140ed02ad532e5675a4692cc4dcec3d2d2db2c
Aidan Bisschops <biss0012@hz.nl>
2025-01-08 16:11:39 +0100

    als je de timer aan het aanpassen bent is start grijs

--- src/lib/timer.svelte
+++ src/lib/timer.svelte
@@ -224,7 +224,12 @@
         {(time % 60).toString().padStart(2, '0')}
     </div>
     {#if !isStarted}
-        <button class="bg-white text-blue-600 border-none px-5 py-2 text-lg rounded-lg cursor-pointer shadow-md transition-colors duration-300 ease-in-out hover:bg-blue-100 mb-1" on:click|stopPropagation={startTimer}>
+        <button 
+            class="bg-white text-blue-600 border-none px-5 py-2 text-lg rounded-lg cursor-pointer shadow-md transition-colors duration-300 ease-in-out hover:bg-blue-100 mb-1" 
+            on:click|stopPropagation={startTimer}
+            disabled={isEditing}
+            style:background-color={isEditing ? '#d3d3d3' : 'white'}
+        >
             Start
         </button>
     {/if}
